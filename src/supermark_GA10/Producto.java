b15f91450dd0df2b558a5b1277173ef266ce3bde
SANTI RUIZ <santiruiz2001@outlook.com>
2022-07-06 21:47:39 -0300

    Los datos de la clase producto se han modificado, se agrego el atributo id y los gt & st

--- src/supermark_GA10/Producto.java
+++ src/supermark_GA10/Producto.java
@@ -3,7 +3,8 @@ package supermark_GA10;
 public class Producto {
 
 	// Atributos
-
+	
+	private Integer id;
 	private String nombre;
 	private String marca;
 	private double precio;
@@ -12,32 +13,65 @@ public class Producto {
 
 	// Metodos
 
-	public Producto(String nombre, String marca, double precio, Integer stock, String vencimiento) {
+	public Producto(Integer id,String nombre, String marca, double precio, Integer stock, String vencimiento) {
+		this.id=id;
 		this.nombre = nombre;
 		this.marca = marca;
 		this.precio = precio;
 		this.stock = stock;
 		this.vencimiento = vencimiento;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
 	}
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	public String getMarca() {
+		return marca;
+	}
+
 	public void setMarca(String marca) {
 		this.marca = marca;
+	}
+
+	public double getPrecio() {
+		return precio;
 	}
 
 	public void setPrecio(double precio) {
 		this.precio = precio;
 	}
 
+	public Integer getStock() {
+		return stock;
+	}
+
 	public void setStock(Integer stock) {
 		this.stock = stock;
+	}
+
+	public String getVencimiento() {
+		return vencimiento;
 	}
 
 	public void setVencimiento(String vencimiento) {
 		this.vencimiento = vencimiento;
 	}
+
+	
+	
+	
 
 }
